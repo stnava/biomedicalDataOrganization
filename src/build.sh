1cb9a63123ba3f6953b7d9ef1bde5498c68c0cd3
stnava <stnava@gmail.com>
2022-10-18 09:31:06 -0400

    ENH: data table

---
+++ src/build.sh
@@ -0,0 +1,5 @@
+
+Rscript -e 'rmarkdown::render( "src/nrg_data_organization_summary.Rmd" ) '
+Rscript -e 'rmarkdown::render( "src/nrg_data_organization_summary.Rmd", output_format="html_document") '
+
+
